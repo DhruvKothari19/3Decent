9ceff7820636b03d6b2542fdbe02338edca51ecf
Dhruv Joshi <joshi.dd@somaiya.edu>
2022-04-19 23:21:16 +0530

    Updated smart contract

--- decentragram/src/contracts/Decentragram.sol
+++ decentragram/src/contracts/Decentragram.sol
@@ -1,21 +1,58 @@
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract Decentragram {
   // Code goes here...
   string public name= "Decentragram";
   //Store Images
+  uint public imageCount = 0;
   mapping(uint => Image) public images;
   struct Image{
     uint id;
     string hash;
     string description;
     uint tipAmount;
-    address payable author memory;
+    address payable author;
   }
+  event ImageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+);
+  event ImageTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+);
+
   //Create Images
-  function uploadImage() public {
-    images[1] = Image(1,'abc', 'Hello World', 0, address());
+  function uploadImage(string memory _imgHash, string memory _description) public {
+    //Image exists
+    require(bytes(_imgHash).length > 0);
+    //Image description exists
+    require(bytes(_description).length > 0);
+    //Uploader address exists
+    require(msg.sender != address(0x0));
+    //Increment image id
+    imageCount++;
+    // Add image to contract
+    images[imageCount] = Image(imageCount,_imgHash, _description, 0, payable(msg.sender));
+
+    emit ImageCreated(imageCount, _imgHash, _description, 0, payable(msg.sender));
+
    }
   //Tip Images
+  function tipImageOwner(uint _id) public payable {
+    require(_id > 0 && _id <= imageCount);
+    Image memory _image = images[_id];
 
+    address _author = _image.author;
+    payable(address(_author)).transfer(msg.value);
+    _image.tipAmount = _image.tipAmount + msg.value;
+    images[_id] = _image; 
+    emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, payable(_author)); 
+  }
 }
