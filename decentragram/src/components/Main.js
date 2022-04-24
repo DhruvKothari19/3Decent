09ca6554ee9c75ee82d0d1b9bd8fa96d40341f4b
DhruvKothari19 <dhruv.ak@somaiya.edu>
2022-04-24 18:32:27 +0530

    [style]

--- decentragram/src/components/Main.js
+++ decentragram/src/components/Main.js
@@ -1,124 +1,135 @@
 import React, { Component } from 'react';
 import Identicon from 'identicon.js';
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './App.css';
 
 class Main extends Component {
 
   render() {
     return (
-      <div className="container-fluid mt-5">
+      <div className="container-fluid mt-1">
         <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
-            <div className="content mr-auto ml-auto">
+          <main role="main" className="col-lg-4 " style={{ maxWidth: '500px'}}>
+            <div className="content">
               <p>&nbsp;</p>
-              <h2>Share Image</h2>
-              <form onSubmit={(event) => {
+              
+              <form className='uploadForm ' onSubmit={(event) => {
                 event.preventDefault()
                 const description = this.imageDescription.value
                 this.props.uploadImage(description)
                 
               }}>
-               
-              <input type='file' accept=".jpg, .jpeg, .png, .bmp, .gif, .mp4" onChange={this.props.captureFile}/>
-              <div className="form-group mr-sm-2">
-              <br></br>
-              <input
+              <h2 className='hello'>What's on Your Mind!?</h2>
+              <input  type='file' id="select__input"accept=".jpg, .jpeg, .png, .bmp, .gif" onChange={this.props.captureFile }/>
+               <input 
                id="imageDescription" 
                type="text"
                ref={(input) => { this.imageDescription = input }}
-               className="form-control"
+               className="form-control mb-3 mt-auto image-des"
                placeholder="Image description..."
                required />
-              </div>
-              <button type="submit" className="btn btn-primary btn-block btn-lg">Upload!</button>
+
+              
+              
+              <button type="submit" className= "upload  mt-3" style={{fontSize: '20px'}} >Upload!</button>
  
               </form>
               
-              { this.props.images.map((image, key) => {
-              return(
-              <div className="card mb-4" key={key} >
-              <div className="card-header">
-                <img
-                  className='mr-2'
-                  width='30'
-                  height='30'
-                  src={ `data:image/png;base64, ${new Identicon(image.author, 30).toString()}`}
-                />
-                <small className="text-muted">{image.author}</small>
-              </div>
-              <ul id="imageList" className="list-group list-group-flush">
-              <li className="list-group-item">
-              <p className="text-center"><img src= {`https://ipfs.infura.io/ipfs/${image.hash}`} style= {{ maxWidth:'420px'}}/> </p>
-              <p>{image.description}</p>
-              </li>
-              <li key={key} className="list-group-item py-2">
-              <small className="float-left mt-1 text-muted">
-              TIPS: {window.web3.utils.fromWei(image.tipAmount.toString(), 'Ether')} ETH </small>
-              <button
-              className="btn btn-link btn-sm float-right pt-0"
-              name={image.id}
+              
+
+            </div>
+          </main>
+          <main role="main" className="col-lg-8 mt-5 mr-auto ml-auto" style={{ maxWidth: '900px' }}>
+            
+            <div className="content mr-auto ml-auto">
+                          
+            { this.props.images.map((image, key) => {
+            return(
+            <div className="card mb-4" key={key} >
+            <div className="card-header">
+              <img
+                className='mr-2'
+                width='30'
+                height='30'
+                src={ `data:image/png;base64, ${new Identicon(image.author, 30).toString()}`}
+              />
+              <small className="text-muted">{image.author}</small>
+            </div>
+            <ul id="imageList" className="list-group list-group-flush">
+            <li className="list-group-item">
+            <p className="text-center"><img src= {`https://ipfs.infura.io/ipfs/${image.hash}`} style= {{ maxWidth:'420px'}}/> </p>
+            <p>{image.description}</p>
+            </li>
+            <li key={key} className="list-group-item py-2">
+            <small className="float-left mt-1 text-muted">
+            TIPS: {window.web3.utils.fromWei(image.tipAmount.toString(), 'Ether')} ETH </small>
+            <button
+            className="btn btn-link btn-sm float-right pt-0"
+            name={image.id}
             onClick={(event) => {
             let tipAmount = window.web3.utils.toWei('0.1', 'Ether')
             console.log(event.target.name, tipAmount)
             this.props.tipImageOwner(event.target.name, tipAmount)
-            
-          }}
-          >
-          TIP 0.1 ETH
 
-                    </button>
-                  </li>
-                </ul>
-                {/* {this.props.comments.map((comment, key)=>{
-                 return(
-                  <div className="card mb-4" key={key} >
-                    <p>{comment.comment}</p>
-                  </div>
-                 )
-                })
-                } */}
-                <form onSubmit={(event) => {
-                event.preventDefault()
-                const addcomment = this.imageComment.value
-                this.props.newComment(addcomment)
-                
-              }}>
-              <div className="form-group mr-sm-2">
-              <br></br>
-              <input
-               id="imageComment" 
-               type="text"
-               ref={(input) => { this.imageComment = input }}
-               className="form-control"
-               placeholder="Add a comment"
-               required />
-              </div>
-              <button type="submit" className="btn btn-primary btn-block btn-lg">Comment!</button>
+            }}
+            >
+            Want to TIP? Click here 
+
+                  </button>
+                </li>
+              </ul>
+              {/* {this.props.comments.map((comment, key)=>{
+              return(
+                <div className="card mb-4" key={key} >
+                  <p>{comment.comment}</p>
+                </div>
+              )
+              })
+              } */}
+              <form onSubmit={(event) => {
+              event.preventDefault()
+              const addcomment = this.imageComment.value
+              this.props.newComment(addcomment)
               
- 
-              </form>
-              
-              { this.props.comments.map((comment, key) => {
-                return(
-                  <div className="card mb-4" key={key} >
-                    <p>{comment.addcomment}</p>
-                  </div>                  
-                )}
-              
-                
+            }}>
+            <div className="form-group mr-sm-2">
+            <br></br>
+            <input
+            id="imageComment" 
+            type="text"
+            ref={(input) => { this.imageComment = input }}
+            className="form-control"
+            placeholder="Add a comment"
+            required />
+            </div>
+            <button type="submit" className="btn btn-primary btn-block btn-lg">Comment!</button>
+
+
+            </form>
+
+            { this.props.comments.map((comment, key) => {
+              return(
+                <div className="card mb-4" key={key} >
+                  <p>{comment.addcomment}</p>
+                </div>                  
               )}
-              </div>
+
+              
+            )}
+            </div>
+            )
+                }
               )
             }
-          )
-        }
             </div>
+            
           </main>
         </div>
       </div>
     );
   }
 }
+
 
 export default Main;
